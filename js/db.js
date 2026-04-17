ad3c15c789f9d3d49efa44005ac801df6c64f6d3
Lei Wang <lwang3@atlassian.com>
2026-04-17 11:15:09 +1000

    security: validate and sanitise book records on read from IndexedDB

--- js/db.js
+++ js/db.js
@@ -24,8 +24,24 @@ function openDB(){
 }
 
 function idbAddBook(record){return new Promise((res,rej)=>{const tx=db.transaction(STORE,'readwrite');tx.oncomplete=()=>res();tx.onerror=()=>rej(tx.error);tx.objectStore(STORE).put(record);});}
-function idbGetAll(){return new Promise((res,rej)=>{const tx=db.transaction(STORE,'readonly');const rq=tx.objectStore(STORE).getAll();rq.onsuccess=()=>res(rq.result||[]);rq.onerror=()=>rej(rq.error);});}
-function idbGet(id){return new Promise((res,rej)=>{const tx=db.transaction(STORE,'readonly');const rq=tx.objectStore(STORE).get(id);rq.onsuccess=()=>res(rq.result||null);rq.onerror=()=>rej(rq.error);});}
+const UUID_REGEX=/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+function sanitiseBook(r){
+  if(!r) return null;
+  return {
+    ...r,
+    id: typeof r.id==='string' ? r.id : String(r.id),
+    title: typeof r.title==='string' ? r.title : '',
+    author: typeof r.author==='string' ? r.author : '',
+    size: typeof r.size==='number' ? r.size : 0,
+    createdAt: typeof r.createdAt==='number' ? r.createdAt : Date.now(),
+    collectionId: typeof r.collectionId==='string' && UUID_REGEX.test(r.collectionId) ? r.collectionId : null,
+    lastCfi: typeof r.lastCfi==='string' ? r.lastCfi : null,
+  };
+}
+
+function idbGetAll(){return new Promise((res,rej)=>{const tx=db.transaction(STORE,'readonly');const rq=tx.objectStore(STORE).getAll();rq.onsuccess=()=>res((rq.result||[]).map(sanitiseBook));rq.onerror=()=>rej(rq.error);});}
+function idbGet(id){return new Promise((res,rej)=>{const tx=db.transaction(STORE,'readonly');const rq=tx.objectStore(STORE).get(id);rq.onsuccess=()=>res(sanitiseBook(rq.result||null));rq.onerror=()=>rej(rq.error);});}
 function idbDelete(id){return new Promise((res,rej)=>{const tx=db.transaction(STORE,'readwrite');tx.objectStore(STORE).delete(id);tx.oncomplete=()=>res();tx.onerror=()=>rej(tx.error);});}
 
 // Collections CRUD
